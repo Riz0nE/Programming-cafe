cd5ff4626a4c1917d8341d773a42393cf72a72bf
rifat rizwan <rifat.rizone.3@gmail.com>
2023-12-05 21:58:17 +0600

    first commit

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Header.css'
+
+const Blog = () => {
+    return (
+        <nav className='header'>
+            <h1>Programming Cafe</h1>
+            <img src="../../../knowledge-cafe/images/profile.png" alt="" />
+        </nav>
+    );
+};
+
+export default Blog;
